53b865c5a9500e6648eff3b89f35966d8ab6bcd0
Miles Exner <dizolivemint@gmail.com>
2020-01-21 13:35:48 -0800

    Update branding

--- header.php
+++ header.php
@@ -1,215 +1,273 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site" style="
-    background: #003b5c;
-    color: #fff;
-">
+    <div id="page" class="site" style="
+		background: #003b5c;
+		color: #fff;
+	">
+        <header id="masthead" class="site-header" role="banner">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <div class="site-branding p-1" style="
+				display: flex;
+				flex-wrap: nowrap;
+				justify-content: space-between;
+				width: 311px;
+			">
+							<div style="
+				height: 60px;
+				width: 60px;
+			">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 144 144"
+                        style="enable-background:new 0 0 144 144;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0 {
+                                fill: #FFFFFF;
+                            }
+                        </style>
+                        <g>
+                            <g>
+                                <g>
+                                    <g>
+                                        <path class="st0" d="M23.94,91.03c0-31.97,26.01-57.98,57.98-57.98c12.36,0,24.34,3.98,34.23,11.23
+					c-1.08-9.47-4.03-16.02-7.05-20.41c-8.19-3.86-17.13-5.9-26.35-5.9c-34.12,0-61.88,27.76-61.88,61.88
+					c0,6.2,0.92,12.3,2.71,18.13c0.42,1.36,1.1,1.74,0.92-0.15C24.22,94.79,23.94,91.27,23.94,91.03z"></path>
+                                        <path class="st0" d="M81.92,39.99c-19.12,0-36.07,10.49-44.81,26.65c-0.13,0.24-0.22,0.56-0.05,0.64
+					c0.4,0.21,0.64-0.22,0.74-0.34c9.07-9.91,22.06-15.87,35.76-15.87c15.52,0,29.31,7.37,38.18,18.75
+					c0.07-0.04,0.15,0.03,0.23,0.13c2.78-5.08,4.33-10.77,4.55-16.7c-0.57-0.37-1.08-0.72-1.23-0.85
+					C106.03,44.4,94.18,39.99,81.92,39.99z"></path>
+                                        <path class="st0" d="M38.59,132.85c-15.3-12.81-24.66-32.14-24.66-53.01c0-37.95,30.87-68.82,68.82-68.82
+					c4.42,0,8.78,0.45,13.05,1.27c-2.26-2.83-8.94-8.32-19.94-9.74c-1.51-0.1-3.04-0.17-4.58-0.17C32.83,2.38,1.66,33.55,1.66,72
+					c0,26.57,14.91,49.62,36.8,61.36C38.71,133.5,39.15,133.27,38.59,132.85z"></path>
+                                        <path class="st0" d="M73.56,58.01c-6.55,0-12.98,1.57-18.76,4.5c-0.49,0.25-0.39,0.59,0.13,0.51c1.58-0.22,3.18-0.33,4.8-0.33
+					c15.44,0,28.52,10.19,32.95,24.18c4.8-1.83,9.22-4.67,12.98-8.45c0.73-0.73,1.4-1.51,2.06-2.29c-0.03-0.08-0.06-0.15-0.09-0.24
+					C100.13,65.11,87.66,58.01,73.56,58.01z"></path>
+                                    </g>
+                                </g>
+                            </g>
+                            <path class="st0" d="M142.34,72c0-25.52-13.74-47.82-34.22-59.94c4.31,3.2,16.13,14.1,16.32,39.47
+		c0.18,24.36-19.37,44.17-43.18,44.17c-15.4,0-29.28-8.1-37.13-21.66l-0.09-0.16l-0.06-0.03c-0.46-0.5-0.95-0.73-1.58-0.73
+		c-1.21,0-2.24,1.01-2.24,2.21c0,0.45,0.13,0.87,0.41,1.29l0.08,0.13c8.7,14.59,23.88,23.31,40.62,23.31
+		c26.22,0,47.55-21.78,47.55-48.55c0-2.88-0.18-5.56-0.56-8.19l0.14-0.03c5.64,12.82,5.22,33.01-5.8,49.63
+		c-14.1,21.27-38.95,28.71-63.32,18.95c-9.44-3.78-17.59-10.18-23.57-18.54c-0.95-1.33-2.02-1.63-3.16-0.89
+		c-0.89,0.58-1.2,1.51-0.86,2.54c0.2,0.6,0.64,1.24,0.96,1.7c0.11,0.16,0.2,0.29,0.3,0.44c6.37,8.45,14.9,14.99,24.68,18.9
+		c14.27,5.72,29.94,5.51,44.1-0.59c14.25-6.14,25.25-17.47,30.97-31.92c3.2-8.09,4.54-16.58,3.97-25.24l0.14-0.02
+		c2.85,12.75,0.88,28.12-2.99,38.05c-9.59,24.56-33.85,40.98-60.09,40.98c-11.62,0-20.88-3.18-23.92-4.22l-0.6-0.2
+		c-0.27-0.12-0.78-0.23-1.1-0.23c-0.99,0-1.72,0.71-1.91,1.87c-0.21,1.32,0.69,2.09,1.24,2.33c7.84,3.06,16.37,4.76,25.29,4.76
+		C111.17,141.62,142.34,110.45,142.34,72z"></path>
+                        </g>
+                    </svg>
+                </div>
+                <div style="margin:auto;">
+                    <p class="site-title m-0" style="
+    					font-size: 21px;
+					">PACIFIC CENTER for
+					<br>
+					LIFELONG LEARNING</p>
+                </div>
+            </div></a><!-- .site-branding -->
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">PACIFIC CENTER for<br/>LIFELONG LEARNING</a></p>
-		</div><!-- .site-branding -->
+            <style>
+                .navigation {
+                    background: #3180c2;
+                    font-family: "Catamaran", "Arial Narrow", Arial, Helvetica, sans-serif;
+                    font-size: 18px;
+                }
 
-		<style>
-		    .navigation {
-		        background: #3180c2;
-		        font-family: "Catamaran", "Arial Narrow", Arial, Helvetica, sans-serif;
-		        font-size: 18px;
-		    }
+                .navigation__list {
+                    zoom: 1;
+                    background: #fff;
+                    margin: 0;
+                    padding: 0;
+                    font-family: "Roboto", "Arial Narrow", Arial, Helvetica, sans-serif;
+                    font-size: 18px;
+                    font-weight: 400;
+                    width: 100%;
+                    display: flex;
+                    flex-wrap: nowrap;
+                    list-style: none;
+                    justify-content: center;
+                }
 
-		    .navigation__list {
-		        zoom: 1;
-		        background: #fff;
-		        margin: 0;
-		        padding: 0;
-		        font-family: "Roboto", "Arial Narrow", Arial, Helvetica, sans-serif;
-		        font-size: 18px;
-		        font-weight: 400;
-		        width: 100%;
-		        display: flex;
-		        flex-wrap: nowrap;
-		        list-style: none;
-			justify-content: center;
-		    }
+                #navigation .navigation__list a {
+                    text-decoration: none;
+                    color: #003b5c;
+                    border-left: 1px solid #003b5c;
+                    padding: 15px 10px;
+                    display: inline-block;
+                    width: 100%;
+                    text-align: center;
+                }
 
-		    #navigation .navigation__list a {
-		        text-decoration: none;
-		        color: #003b5c;
-		        border-left: 1px solid #003b5c;
-		        padding: 15px 10px;
-		        display: inline-block;
-		        width: 100%;
-		        text-align: center;
-		    }
+                #navigation .navigation__list .navigation__list__last {
+                    border-right: 1px solid #003b5c;
+                }
 
-			#navigation .navigation__list .navigation__list__last {
-				border-right: 1px solid #003b5c;
-			}
+                #navigation .navigation__list a:hover {
+                    background-color: #003b5c;
+                    color: #fff;
+                }
 
-		    #navigation .navigation__list a:hover {
-		        background-color: #003b5c;
-		        color: #fff;
-		    }
+                #navigation .navigation__list li {
+                    width: 16.6%;
+                }
 
-		    #navigation .navigation__list li {
-		        width: 16.6%;
-		    }
+                .navigation__list ul {
+                    list-style: none;
+                    z-index: 1000;
+                }
 
-		    .navigation__list ul {
-		        list-style: none;
-		        z-index: 1000;
-		    }
+                .navigation__drop {
+                    position: absolute;
+                    width: 16.6%;
+                    display: block;
+                }
 
-		    .navigation__drop {
-		        position: absolute;
-		        width: 16.6%;
-		        display: block;
-		    }
+                #navigation ul.navigation__drop li {
+                    width: 100%;
+                    background: #3180c2;
+                }
 
-		    #navigation ul.navigation__drop li {
-		        width: 100%;
-		        background: #3180c2;
-		    }
+                input.navigation__checkbox {
+                    display: none;
+                }
 
-		    input.navigation__checkbox {
-		        display: none;
-		    }
+                .navigation__button {
+                    background-color: #3180c2;
+                    position: relative;
+                    margin: 0 auto;
+                    z-index: 2000;
+                    text-align: center;
+                    cursor: pointer;
+                }
 
-		    .navigation__button {
-		        background-color: #3180c2;
-		        position: relative;
-		        margin: 0 auto;
-		        z-index: 2000;
-		        text-align: center;
-		        cursor: pointer;
-		    }
+                .navigation__icon {
+                    position: relative;
+                    margin-top: 22px;
+                }
 
-		    .navigation__icon {
-		        position: relative;
-		        margin-top: 22px;
-		    }
+                .navigation__icon,
+                .navigation__icon::before,
+                .navigation__icon::after {
+                    width: 2rem;
+                    height: 4px;
+                    background-color: #fff;
+                    display: inline-block;
+                }
 
-		    .navigation__icon,
-		    .navigation__icon::before,
-		    .navigation__icon::after {
-		        width: 2rem;
-		        height: 4px;
-		        background-color: #fff;
-		        display: inline-block;
-		    }
+                .navigation__icon::before,
+                .navigation__icon::after {
+                    content: "";
+                    position: absolute;
+                    left: 0;
+                    transition: all 0.2s;
+                }
 
-		    .navigation__icon::before,
-		    .navigation__icon::after {
-		        content: "";
-		        position: absolute;
-		        left: 0;
-		        transition: all 0.2s;
-		    }
+                .navigation__icon::before {
+                    top: -0.8rem;
+                }
 
-		    .navigation__icon::before {
-		        top: -0.8rem;
-		    }
+                .navigation__icon::after {
+                    top: 0.8rem;
+                }
 
-		    .navigation__icon::after {
-		        top: 0.8rem;
-		    }
+                .navigation__button:hover .navigation__icon::before {
+                    top: -1rem;
+                }
 
-		    .navigation__button:hover .navigation__icon::before {
-		        top: -1rem;
-		    }
+                .navigation__button:hover .navigation__icon::after {
+                    top: 1rem;
+                }
 
-		    .navigation__button:hover .navigation__icon::after {
-		        top: 1rem;
-		    }
+                .navigation__checkbox:checked+.navigation__button .navigation__icon {
+                    background-color: transparent;
+                }
 
-		    .navigation__checkbox:checked+.navigation__button .navigation__icon {
-		        background-color: transparent;
-		    }
+                .navigation__checkbox:checked+.navigation__button .navigation__icon::before {
+                    top: 0;
+                    transform: rotate(135deg);
+                }
 
-		    .navigation__checkbox:checked+.navigation__button .navigation__icon::before {
-		        top: 0;
-		        transform: rotate(135deg);
-		    }
+                .navigation__checkbox:checked+.navigation__button .navigation__icon::after {
+                    top: 0;
+                    transform: rotate(-135deg);
+                }
 
-		    .navigation__checkbox:checked+.navigation__button .navigation__icon::after {
-		        top: 0;
-		        transform: rotate(-135deg);
-		    }
+                .navigation label {
+                    user-select: none;
+                }
 
-		    .navigation label {
-		        user-select: none;
-		    }
+                .navigation__mobile {
+                    display: none;
+                }
 
-		    .navigation__mobile {
-		        display: none;
-		    }
+                @media all and (max-width: 1092px) {
+                    .navigation__mobile {
+                        display: block;
+                    }
 
-		    @media all and (max-width: 1092px) {
-		        .navigation__mobile {
-		            display: block;
-		        }
+                    #navigation .navigation__list li {
+                        width: 100%;
+                        padding: 0;
+                    }
 
-		        #navigation .navigation__list li {
-		            width: 100%;
-		            padding: 0;
-		        }
+                    .navigation__list li a {
+                        padding: 1rem 0;
+                        border-left: none;
+                        border-top: 1px solid #2c73af;
+                    }
 
-		        .navigation__list li a {
-		            padding: 1rem 0;
-		            border-left: none;
-		            border-top: 1px solid #2c73af;
-		        }
+                    .navigation__drop {
+                        position: relative;
+                        width: 100%;
+                        border: 2px solid #fff;
+                    }
 
-		        .navigation__drop {
-		            position: relative;
-		            width: 100%;
-		            border: 2px solid #fff;
-		        }
+                    .navigation__checkbox~.navigation__list {
+                        display: none;
+                    }
 
-		        .navigation__checkbox~.navigation__list {
-		            display: none;
-		        }
+                    .navigation__checkbox:checked~.navigation__list {
+                        display: block;
+                    }
+                }
 
-		        .navigation__checkbox:checked~.navigation__list {
-		            display: block;
-		        }
-		    }
+                .hidden {
+                    display: none;
+                }
+            </style>
+            <nav id="navigation" class="navigation" role="navigation">
+                <input #navigation type="checkbox" class="navigation__checkbox" id="navi-toggle">
+                <label for="navi-toggle" class="navigation__mobile navigation__button">
+                    <span class="navigation__icon">&nbsp;</span>
+                </label>
 
-		    .hidden {
-		        display: none;
-		    }
-		</style>
-		<nav id="navigation" class="navigation" role="navigation">
-			<input #navigation type="checkbox" class="navigation__checkbox" id="navi-toggle">
-			<label for="navi-toggle" class="navigation__mobile navigation__button">
-			    <span class="navigation__icon">&nbsp;</span>
-			</label>
-
-			<ul class="navigation__list">
-			        <li class="first"><a href="/joomdle/login">My Account</a></li>
-			        <li><a href="/joomdle/courses">Course Library</a></li>
-			        <!-- li class="parent"><a href="#">Pacific Symposium</a>
+                <ul class="navigation__list">
+                    <li class="first"><a href="/joomdle/login">My Account</a></li>
+                    <li><a href="/joomdle/courses">Course Library</a></li>
+                    <!-- li class="parent"><a href="#">Pacific Symposium</a>
 			            <ul class="navigation__list navigation__drop hidden">
 			                <li><a href="https://www.pacificcollege.edu/symposium">Live in San Diego</a></li>
 			                <li><a href="/joomdle/pacific-symposium/streaming">Live Online</a></li>
 			            </ul>
 			        </li -->
-			        <li><a href="/joomdle/course-access">My Courses</a></li>
-			        <li><a href="/joomdle/support">Support</a></li>
-			        <li class="navigation__list__last"><a href="https://pacificcenterforlifelonglearning.com/library/podcast">Podcast</a></li>
-    			</ul>
-		</nav><!-- #site-navigation -->
-		<script>
-		    "use strict"; !(function () { var e = document.getElementsByClassName("parent")[0], n = document.getElementsByClassName("navigation__drop")[0]; e.addEventListener("click", (function () { n.classList.toggle("hidden") })), e.addEventListener("mouseover", (function () { n.classList.contains("hidden") || n.classList.remove("hidden") })), e.addEventListener("mouseout", (function () { n.classList.contains("hidden") && n.classList.add("hidden") })) })();
-		</script>
-	</header><!-- #masthead -->
+                    <li><a href="/joomdle/course-access">My Courses</a></li>
+                    <li><a href="/joomdle/support">Support</a></li>
+                    <li class="navigation__list__last"><a
+                            href="https://pacificcenterforlifelonglearning.com/library/podcast">Podcast</a></li>
+                </ul>
+            </nav><!-- #site-navigation -->
+            <script>
+                "use strict"; !(function () { var e = document.getElementsByClassName("parent")[0], n = document.getElementsByClassName("navigation__drop")[0]; e.addEventListener("click", (function () { n.classList.toggle("hidden") })), e.addEventListener("mouseover", (function () { n.classList.contains("hidden") || n.classList.remove("hidden") })), e.addEventListener("mouseout", (function () { n.classList.contains("hidden") && n.classList.add("hidden") })) })();
+            </script>
+        </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+        <div id="content" class="site-content">
