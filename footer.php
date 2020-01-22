681462da57c28063b9f3db9ee38af1eb87172af5
Miles Exner <dizolivemint@gmail.com>
2020-01-22 15:32:10 -0800

    Update footer

--- footer.php
+++ footer.php
@@ -1,9 +1,75 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		
-	</footer><!-- #colophon -->
+	<footer id="footer" style="background-color: #565656; color:white;">
+    <div class="container">
+        <div class="grid">
+            <div class="block footer-menu size-50">
+                <div class="content particle">
+                    <nav class="main-nav menu-hastouch" role="navigation" data-mobile-target=""
+                        data-hover-expand="false">
+                        <ul class="toplevel">
+                            <li class="menu-item menu-item-type-component menu-item-239 standard  "
+                                title="About Us">
+                                <a class="menu-item-container" href="/joomdle/about">
+                                    <span class="menu-item-content">
+                                        <span class="menu-item-title">About Us</span>
+
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="menu-item menu-item-type-component menu-item-241 standard  " title="FAQ">
+                                <a class="menu-item-container" href="/joomdle/faq">
+                                    <span class="menu-item-content">
+                                        <span class="menu-item-title">FAQ</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="menu-item menu-item-type-component menu-item-242 standard  "
+                                title="Contact Us">
+                                <a class="menu-item-container" href="/joomdle/contact-us">
+                                    <span class="menu-item-content">
+                                        <span class="menu-item-title">Contact Us</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="menu-item menu-item-type-url menu-item-418 standard  "
+                                title="Pacific College">
+                                <a class="menu-item-container" href="https://www.pacificcollege.edu/">
+                                    <span class="menu-item-content">
+                                        <span class="menu-item-title">Pacific College</span>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="block size-50">
+                <div class="content particle">
+                    <div class="social social-items">
+                        <a target="_blank" href="https://twitter.com/PacificLifeLong" title="" aria-label="">
+                            <span class="fa fa-twitter fa-2x"></span> <span class="social-text"></span> </a>
+                        <a target="_blank" href="https://www.facebook.com/pacificcollege.lifelong" title=""
+                            aria-label="">
+                            <span class="fa fa-facebook fa-2x"></span> <span class="social-text"></span> </a>
+                        <a target="_blank" href="https://www.youtube.com/user/PCOMelearning" title="" aria-label="">
+                            <span class="fa fa-youtube-play fa-2x"></span> <span class="social-text"></span> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="grid">
+            <div class="block size-100">
+                <div class="content particle">
+                    ©
+                    2020
+                    Pacific Center for Lifelong Learning
+                </div>
+            </div>
+        </div>
+    </div>
+</footer><!-- #footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
