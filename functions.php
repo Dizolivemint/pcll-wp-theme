cdf0f74f5d31624c326a45266017b24837ae35c0
Miles Exner <dizolivemint@gmail.com>
2020-01-23 12:55:02 -0800

    Init woocommerce cart

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@
 		wp_deregister_script( 'wc-add-to-cart' );
 		wp_deregister_script( 'wc-cart-fragments' );
 		wp_deregister_script( 'woocommerce' );
-                wp_deregister_script( 'woocommerce_integration' );
+        wp_deregister_script( 'woocommerce_integration' );
 		if (is_admin()) return;
 		wp_deregister_script( 'jquery' );
 		wp_deregister_script( 'jquery-core' );
@@ -73,5 +73,26 @@
 		wp_deregister_script( 'jquery-ui-mouse' );
 		wp_deregister_script( 'jquery-ui-position' );
 		wp_deregister_script( 'jquery-ui-resizable' );
-                wp_deregister_script( 'jquery-ui-widget' );
+        wp_deregister_script( 'jquery-ui-widget' );
 	}
+	/**
+	 * Ensure cart contents update when products are added to the cart via AJAX
+	 */
+	function my_header_add_to_cart_fragment( $fragments ) {
+	
+		ob_start();
+		$count = WC()->cart->cart_contents_count;
+		?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+		if ( $count > 0 ) {
+			?>
+			<span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+			<?php            
+		}
+			?></a><?php
+	
+		$fragments['a.cart-contents'] = ob_get_clean();
+		
+		return $fragments;
+	}
+	add_filter( 'woocommerce_add_to_cart_fragments', 'my_header_add_to_cart_fragment' );
+
--- header.php
+++ header.php
@@ -84,7 +84,18 @@
 					Search
 				</div>
 				<div class="d-flex col-sm align-items-center justify-content-end header__w-50-xs mx-auto">
-					Cart
+				<?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+ 
+					$count = WC()->cart->cart_contents_count;
+					?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php 
+					if ( $count > 0 ) {
+						?>
+							<span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+						<?php
+					}
+						?></a>
+
+					<?php } ?>
 				</div>
             </div><!-- .site-branding -->
 
