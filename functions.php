707fe3ea524ee6cc0532b35c3029d188b83cc486
Miles Exner <dizolivemint@gmail.com>
2020-01-23 15:38:12 -0800

    Fix broken heart, I mean cart

--- functions.php
+++ functions.php
@@ -82,7 +82,7 @@
 	
 		ob_start();
 		$count = WC()->cart->cart_contents_count;
-		?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+		?><a class="cart-contents icon-shopping_cart" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
 		if ( $count > 0 ) {
 			?>
 			<span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
