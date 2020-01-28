db7bebb921cce1280503952823c522004fa37c6b
Miles Exner <dizolivemint@gmail.com>
2020-01-28 12:07:04 -0800

    Add front page

---
+++ front-page.php
@@ -0,0 +1,67 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 p-0 mx-auto">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="wp-content/uploads/slider3/spices-2548653_1920x600.jpeg"
+                            alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="wp-content/uploads/slider3/spices-2548653_1920x600.jpeg"
+                            alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="wp-content/uploads/slider3/spices-2548653_1920x600.jpeg"
+                            alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 border round p-1 mx-auto">
+            <!--Filter
+    <select id="selectbox" name="" onchange="Array.from(document.getElementsByClassName('location')).forEach(function(item) { item.classList.add('hidden'); });document.getElementById(this.value).classList.remove('hidden');">
+    <option value="" selected>Location</option>
+    <option value="OnDemand">On-Demand</option>
+    <option value="LiveWebinar">Live Webinar</option>
+    <option value="NewYork">New York</option>
+    <option value="Chicago">Chicago</option>
+    <option value="SanDiego">San Diego</option>
+    </select>
+    <div id="OnDemand" class="location hidden">
+    [products columns="2" attribute="Location" terms="On-Demand" orderby="date"]</div>
+    <div id="LiveWebinar" class="location hidden">
+    [products columns="2" attribute="Location" terms="Live Webinar" orderby="date"]</div>
+    <div id="NewYork" class="location hidden">
+    [products columns="2" attribute="Location" terms="New York" orderby="date"]</div>
+    <div id="Chicago" class="location hidden">
+    [products columns="2" attribute="Location" terms="Chicago" orderby="date"]</div>
+    <div id="SanDiego" class="location hidden">
+    [products columns="2" attribute="Location" terms="San Diego" orderby="date"]</div>
+    End Filter-->
+            <h2>Welcome to Pacific Center for Lifelong Learning!</h2>
+            Pacific Center is dedicated to the principle that learning is fun and enriches your enjoyment of life.
+
+            Here is a place to nurture your sense of wonder at the world and to enjoy the satisfaction of discovery.
+            Pacific Center offers a wide range of <a
+                href="https://pacificcenterforlifelonglearning.com/joomdle/courses">continuing education courses</a> for
+            acupuncturists and other healthcare practitioners. Popular topics include Women's Health, Advanced Herbal
+            Medicine, Functional Medicine, and the use of Western herbs according to the principles of TCM.
+        </div>
+    </div>
+</div>
