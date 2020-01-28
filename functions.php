5c91dce442dc39112205571cc086e7ec4021cef1
Miles Exner <dizolivemint@gmail.com>
2020-01-27 16:15:37 -0800

    Add search

--- functions.php
+++ functions.php
@@ -96,3 +96,87 @@
 	}
 	add_filter( 'woocommerce_add_to_cart_fragments', 'my_header_add_to_cart_fragment' );
 
+	require get_theme_file_path('/inc/search-route.php');
+
+	function pcll_custom_rest() {
+	  register_rest_field('post', 'authorName', array(
+		'get_callback' => function() {return get_the_author();}
+	  ));
+	}
+	
+	add_action('rest_api_init', 'pcll_custom_rest');
+	
+	function pageBanner($args = NULL) {
+	  
+	  if (!$args['title']) {
+		$args['title'] = get_the_title();
+	  }
+	
+	  if (!$args['subtitle']) {
+		$args['subtitle'] = get_field('page_banner_subtitle');
+	  }
+	
+	  if (!$args['photo']) {
+		if (get_field('page_banner_background_image')) {
+		  $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+		} else {
+		  $args['photo'] = get_theme_file_uri('/images/pclldefaultbg.jpg');
+		}
+	  }
+	
+	  ?>
+	  <div class="page-banner">
+		<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+		<div class="page-banner__content container container--narrow">
+		  <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+		  <div class="page-banner__intro">
+			<p><?php echo $args['subtitle']; ?></p>
+		  </div>
+		</div>  
+	  </div>
+	<?php }
+	
+	function pcll_files() {
+	  wp_localize_script('main-pcll-js', 'pcllData', array(
+		'root_url' => get_site_url()
+	  ));
+	}
+	
+	add_action('wp_enqueue_scripts', 'pcll_files');
+	
+	function pcll_features() {
+	  add_theme_support('title-tag');
+	  add_theme_support('post-thumbnails');
+	  add_image_size('instructorLandscape', 400, 260, true);
+	  add_image_size('instructorPortrait', 480, 650, true);
+	  add_image_size('pageBanner', 1500, 350, true);
+	}
+	
+	add_action('after_setup_theme', 'pcll_features');
+	
+	function pcll_adjust_queries($query) {
+	  if (!is_admin() AND is_post_type_archive('podcast') AND is_main_query()) {
+		$query->set('meta_key', 'event_date');
+		$query->set('orderby', 'meta_value_num');
+		$query->set('orderby', 'event_date');
+		$query->set('order', 'ASC');
+		$query->set('posts_per_page', -1);
+	  }
+	
+	  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+		$today = date('Ymd');
+		$query->set('meta_key', 'event_date');
+		$query->set('orderby', 'meta_value_num');
+		$query->set('order', 'ASC');
+		$query->set('meta_query', array(
+				  array(
+					'key' => 'event_date',
+					'compare' => '>=',
+					'value' => $today,
+					'type' => 'numeric'
+				  )
+				));
+	  }
+	}
+	
+	add_action('pre_get_posts', 'pcll_adjust_queries');
---
+++ inc/search-route.php
@@ -0,0 +1,146 @@
+<?php
+
+add_action('rest_api_init', 'pcllRegisterSearch');
+
+function pcllRegisterSearch() {
+  register_rest_route('pcll/v1', 'search', array(
+    'methods' => WP_REST_SERVER::READABLE,
+    'callback' => 'pcllSearchResults'
+  ));
+}
+
+function pcllSearchResults($data) {
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post', 'page', 'instructor', 'product', 'podcast', 'event'),
+    's' => sanitize_text_field($data['term'])
+  ));
+
+  $results = array(
+    'generalInfo' => array(),
+    'instructors' => array(),
+    'products' => array(),
+    'events' => array(),
+    'podcasts' => array()
+  );
+
+  while($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'postType' => get_post_type(),
+        'authorName' => get_the_author()
+      ));
+    }
+
+    if (get_post_type() == 'instructor') {
+      array_push($results['instructors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'image' => get_the_post_thumbnail_url(0, 'instructorLandscape')
+      ));
+    }
+
+    if (get_post_type() == 'product') {
+      $relatedPodcasts = get_field('related_podcast');
+
+      if ($relatedPodcasts) {
+        foreach($relatedPodcasts as $podcast) {
+          array_push($results['podcasts'], array(
+            'title' => get_the_title($podcast),
+            'permalink' => get_the_permalink($podcast)
+          ));
+        }
+      }
+    
+      array_push($results['products'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'id' => get_the_id()
+      ));
+    }
+
+    if (get_post_type() == 'podcast') {
+      array_push($results['podcasts'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'event') {
+      $eventDate = new DateTime(get_field('event_date'));
+      $description = null;
+      if (has_excerpt()) {
+        $description = get_the_excerpt();
+      } else {
+        $description = wp_trim_words(get_the_content(), 18);
+      }
+
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'month' => $eventDate->format('M'),
+        'day' => $eventDate->format('d'),
+        'description' => $description
+      ));
+    }
+    
+  }
+
+  if ($results['products']) {
+    $productsMetaQuery = array('relation' => 'OR');
+
+    foreach($results['products'] as $item) {
+      array_push($productsMetaQuery, array(
+          'key' => 'related_products',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+    }
+
+    $productRelationshipQuery = new WP_Query(array(
+      'post_type' => array('instructor', 'event'),
+      'meta_query' => $productsMetaQuery
+    ));
+
+    while($productRelationshipQuery->have_posts()) {
+      $productRelationshipQuery->the_post();
+
+      if (get_post_type() == 'event') {
+        $eventDate = new DateTime(get_field('event_date'));
+        $description = null;
+        if (has_excerpt()) {
+          $description = get_the_excerpt();
+        } else {
+          $description = wp_trim_words(get_the_content(), 18);
+        }
+
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'month' => $eventDate->format('M'),
+          'day' => $eventDate->format('d'),
+          'description' => $description
+        ));
+      }
+
+      if (get_post_type() == 'instructor') {
+        array_push($results['instructors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'instructorLandscape')
+        ));
+      }
+
+    }
+
+    $results['instructors'] = array_values(array_unique($results['instructors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+  }
+
+
+  return $results;
+
+}
