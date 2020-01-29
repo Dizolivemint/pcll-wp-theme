69fc623fa033c5d330e30b225951bbfa330506d2
Miles Exner <dizolivemint@gmail.com>
2020-01-29 15:50:41 -0800

    Update instructor page template

--- single-instructor.php
+++ single-instructor.php
@@ -10,51 +10,55 @@
 
     <div class="container page-section">
           
-      <div class="row group">
+      <div class="row">
 
           <div class="col-sm">
-            <img src="<?php get_field('instructor_image'); ?>">
+          <?php $instructorImage = get_field('instructor_image'); ?>
+            <img src="<?php $instructorImage ?>"/>
           </div>
 
           <div class="col-sm">
             <?php the_content(); ?>
-            <?php echo do_shortcode('[Sassy_Social_Share style="background-color:#000;"]') ?>
+            
           </div>
 
       </div>
+      <hr class="section-break">
+      <?php echo do_shortcode('[Sassy_Social_Share style="background-color:#000;"]') ?>
+      
+      <div class="row">
+        <?php
 
-      <?php
+          $relatedProducts = get_field('related_products');
 
-        $relatedProducts = get_field('related_products');
+          if ($relatedProducts) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Course(s)</h2>';
+            echo '<ul class="link-list min-list">';
+            foreach($relatedProducts as $product) { ?>
+              <li><a href="<?php echo get_the_permalink($product); ?>"><?php echo get_the_title($product); ?></a></li>
+            <?php }
+            echo '</ul>';
+          }
 
-        if ($relatedProducts) {
-          echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Course(s)</h2>';
-          echo '<ul class="link-list min-list">';
-          foreach($relatedProducts as $product) { ?>
-            <li><a href="<?php echo get_the_permalink($product); ?>"><?php echo get_the_title($product); ?></a></li>
-          <?php }
-          echo '</ul>';
-        }
+        ?>
 
-      ?>
+        <?php
 
-      <?php
+          $relatedPodcasts = get_field('related_podcasts');
 
-        $relatedPodcasts = get_field('related_podcasts');
+          if ($relatedPodcasts) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Course(s)</h2>';
+            echo '<ul class="link-list min-list">';
+            foreach($relatedPodcasts as $podcast) { ?>
+              <li><a href="<?php echo get_the_permalink($podcast); ?>"><?php echo get_the_title($podcast); ?></a></li>
+            <?php }
+            echo '</ul>';
+          }
 
-        if ($relatedPodcasts) {
-          echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Course(s)</h2>';
-          echo '<ul class="link-list min-list">';
-          foreach($relatedPodcasts as $podcast) { ?>
-            <li><a href="<?php echo get_the_permalink($podcast); ?>"><?php echo get_the_title($podcast); ?></a></li>
-          <?php }
-          echo '</ul>';
-        }
-
-      ?>
-
+        ?>
+      </div>
     </div>
     
   <?php }
