fec1a4112475719b9806882e8eec92fc42f3a297
Miles Exner <dizolivemint@gmail.com>
2020-01-29 15:40:27 -0800

    Add instructor page template

---
+++ single-instructor.php
@@ -0,0 +1,64 @@
+<?php
+  
+  get_header();
+
+  while(have_posts()) {
+    the_post();
+    pageBanner();
+     ?>
+    
+
+    <div class="container page-section">
+          
+      <div class="row group">
+
+          <div class="col-sm">
+            <img src="<?php get_field('instructor_image'); ?>">
+          </div>
+
+          <div class="col-sm">
+            <?php the_content(); ?>
+            <?php echo do_shortcode('[Sassy_Social_Share style="background-color:#000;"]') ?>
+          </div>
+
+      </div>
+
+      <?php
+
+        $relatedProducts = get_field('related_products');
+
+        if ($relatedProducts) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Course(s)</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedProducts as $product) { ?>
+            <li><a href="<?php echo get_the_permalink($product); ?>"><?php echo get_the_title($product); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+
+      ?>
+
+      <?php
+
+        $relatedPodcasts = get_field('related_podcasts');
+
+        if ($relatedPodcasts) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Course(s)</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPodcasts as $podcast) { ?>
+            <li><a href="<?php echo get_the_permalink($podcast); ?>"><?php echo get_the_title($podcast); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+
+      ?>
+
+    </div>
+    
+  <?php }
+
+  get_footer();
+
+?>
