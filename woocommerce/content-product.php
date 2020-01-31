afa45f74933b87e5e9413e5138a40978a106b355
Miles Exner <dizolivemint@gmail.com>
2020-01-31 14:52:38 -0800

    Test fields

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -72,22 +72,22 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 
 	/* Additional product information rendered */
 
-	echo ('<ul>');
-	echo ('<li>', get_field('number_of_ceus'), 'CEUs/PDAs');
-	echo (' (NCCAOM ', $nccaom);
+	echo '<ul>';
+	echo '<li>', get_field('number_of_ceus'), 'CEUs/PDAs';
+	echo ' (NCCAOM ', $nccaom;
 	if ($cab) {
-		echo ('and CAB ', $cab)
+		echo 'and CAB ', $cab;
 	}
-	echo (')</li>')
+	echo ')</li>';
 	
 	if ($cab) {
-		echo ('<li>', 'CAB Category ', $cabCat, '</li>');
+		echo '<li>', 'CAB Category ', $cabCat, '</li>';
 	}
 	
 	if ($handouts) {
-		echo ('<li>Handouts Included</li>')
+		echo '<li>Handouts Included</li>';
 	}
-   	echo ('</ul>');
+   	echo '</ul>';
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item_title.
