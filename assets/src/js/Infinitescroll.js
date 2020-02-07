77e3be0caf7c78c9af421c9a34c271ea15040d79
Miles Exner <dizolivemint@gmail.com>
2020-02-07 12:43:06 -0800

    Add infinite scroll

--- assets/src/js/Infinitescroll.js
+++ assets/src/js/Infinitescroll.js
@@ -1,11 +1,14 @@
 var InfiniteScroll = require('infinite-scroll');
 
-// $( document ).ready(function() {
-//     var infScroll = new InfiniteScroll( '.container', { 
-//         // options
-//         path: '.next',
-//         append: '.site-main'
-//     });
-// });
+$( document ).ready(function() {
+    var infScroll = new InfiniteScroll( '.container', { 
+        // options
+        path: '.next',
+        append: '.products',
+        history: 'false',
+        checkLastPage: true,
+        responseType: 'document'
+    });
+});
 
 
