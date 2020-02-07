5818431ea69b5cef0f79b92a850cb2a9042afb74
Miles Exner <dizolivemint@gmail.com>
2020-02-07 12:50:32 -0800

    Add infinite scroll

--- assets/src/js/Infinitescroll.js
+++ assets/src/js/Infinitescroll.js
@@ -1,15 +1,14 @@
 var InfiniteScroll = require('infinite-scroll');
 
 $( document ).ready(function() {
-    var infScroll = new InfiniteScroll( '.container', { 
+    var infScroll = new InfiniteScroll( '.products', { 
         // options
         path: '.next',
         append: '.products',
         history: 'false',
         checkLastPage: true,
         responseType: 'document',
-        hideNav: '.woocommerce-pagination',
-        elementScroll: true
+        hideNav: '.woocommerce-pagination'
     });
 });
 
